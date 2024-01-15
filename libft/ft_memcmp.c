b626602d7d857e2c85ed87904deb804daa68e792
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-01-15 16:19:12 +0100

    memcmp correction prototype

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_memcmp(void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t				i;
 	const unsigned char	*u1;
