b56d60699155d693b570e55224d3ae00eba5079f
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-01-15 14:23:55 +0100

    strncmp size_t

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -10,9 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
 	if (!n)
