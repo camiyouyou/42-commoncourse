60aa151c79859e2b53769ec41fcc9749be2147a9
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-01-15 19:02:33 +0100

    Update ft_isascii.c
    chnaged proto

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -10,20 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isascii(char *str)
+int	ft_isascii(int c)
 {
-	int	i;
-	int	c;
-
-	i = 0;
-	while (str[i] != '\0')
+	if (!(c >= 0 && c <= 127))
 	{
-		c = str[i];
-		if (!(c >= 0 && c <= 127))
-		{
-			return (0);
-		}
-		i++;
+		return (0);
 	}
 	return (1);
 }
