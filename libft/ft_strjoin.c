4e569216d2d0997e4842acc1f1366d8937efe697
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-01-16 19:33:13 +0100

    Update ft_strjoin.c
    changed dynamic and fixed syntax errors

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -32,13 +32,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
 	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str < 0)
+	if (str == NULL)
 		return (NULL);
 	while (i < len && s1[i] != '\0')
 	{
-		str[i] = *s1[i];
+		str[i] = s1[i];
   		i++;
 	}
 	j = 0;
@@ -47,7 +47,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		str[i + j] = s2[j];
 		j++;
 	}
-	if (i < len)
-		dest[i + j] = '\0';
+	str[i + j] = '\0';
 	return (str); 
 }
