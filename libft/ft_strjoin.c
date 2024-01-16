24fe05682e8bd849a742d915f4abee2c5a0e3c04
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-01-16 19:10:50 +0100

    Update ft_strjoin.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -32,7 +32,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	s = 0;
 	len = ft_strlen(s1) + ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str < 0)
@@ -49,12 +48,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		j++;
 	}
 	if (i < len)
-	{
 		dest[i + j] = '\0';
-	}
-	while (src[j] != '\0')
-	{
-		j++;
-	} 
 	return (str); 
 }
