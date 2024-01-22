cf3def83525d4aa419889db8a21194465f46fe65
Camille Rousseau <camille@MacBook-Pro-de-Camille.local>
2024-01-22 11:30:15 +0100

    minor changes to fit francinette

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,20 +6,20 @@
 /*   By: croussea <croussea@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:20:53 by croussea          #+#    #+#             */
-/*   Updated: 2024/01/19 16:34:48 by croussea         ###   ########.fr       */
+/*   Updated: 2024/01/19 17:27:29 by croussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
+	if ((char)c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
-	if (c == '\0')
-		return ((char *)s);
 	return (0);
 }
