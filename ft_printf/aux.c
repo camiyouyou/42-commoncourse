d7fc8d1a73ef00a3b05d6d5b6525e5f470c14c1b
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-02-02 14:14:59 +0100

    d2but du projet

---
+++ ft_printf/aux.c
@@ -0,0 +1,42 @@
+
+void    ft_putnbr_fd(int n, int fd)
+{
+    if (n == -2147483648)
+    {
+        write(fd, "-2147483648", 11);
+        return ;
+    }
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n = -n;
+    }
+    if (n > 9)
+    {
+        ft_putnbr_fd((n / 10), fd);
+        ft_putnbr_fd((n % 10), fd);
+    }
+    else
+    {
+        ft_putchar_fd(n + 48, fd);
+    }
+}
+
+void    ft_putstr_fd(char *s, int fd)
+{
+    int    i;
+
+    i = 0;
+    while (s[i] != '\0')
+    {
+        ft_putchar_fd(s[i], fd);
+        i++;
+    }
+}
+
+void    ft_putchar_fd(char c, int fd)
+{
+    if (ft_isascii(c))
+        write (fd, &c, 1);
+}
+
---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,12 @@
+int ft_printf(const char *, ...)
+{
+    // si le flag est c, utiliser ft_putchar
+    //si le flag est s, putstring
+    //si le flag est p --> custom fonction pour hexadecimal
+    //si le flag est d, putnbr
+    //si i le flag est i, putnbr
+    //si le le flag est u, faire iun putnbr juste pour positifs
+    // pour x et X custom fonction hexadecimale
+    //%% --> ft_putchar %
+    
+}
