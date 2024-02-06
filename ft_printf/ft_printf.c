9f277c35def2329edf4f0865b4d1e68d4a931d08
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-02-06 19:17:02 +0100

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,5 +1,24 @@
-int ft_printf(const char *, ...)
+int ft_printf(const char *s, ...)
 {
+    int i;
+    va_list ap;
+
+    va_start(ap, s);
+    i = 0;
+    while (s)
+    {
+        if (*s == '%')
+        {
+            i++;
+            if (*s == c)
+            {
+                ft_putchar_ap(va_arg(s, ap));
+            }
+        }
+        ft_putchar(s[i]);
+    }
+    va_end(ap, s);
+    
     // si le flag est c, utiliser ft_putchar
     //si le flag est s, putstring
     //si le flag est p --> custom fonction pour hexadecimal
