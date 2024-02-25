2cf9a3a187c400c9c94c16d94f480922460e18c1
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-02-25 21:23:49 +0100

    Update aux.c

--- ft_printf/aux.c
+++ ft_printf/aux.c
@@ -58,3 +58,16 @@ int    ft_hexa(unsigned long n, char format)
     return (i);
 }
 
+int	ft_putunsigned(unsigned long n)
+{
+	int		len;
+	char	*base;
+
+	len = 0;
+	base = "0123456789";
+	if (n > (ft_strlen(base) - 1))
+		len += ft_putunsigned(n / ft_strlen(base));
+	len += ft_putchar(*(base + (n % ft_strlen(base))));
+	return (len);
+}
+
