a3c1e98e14f34232868c2823522881253ec4e733
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-02-26 19:16:46 +0100

    Update ft_printf.c

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,40 +1,22 @@
 int ft_printf(const char *s, ...)
 {
-    int i;
     va_list ap;
 
     va_start(ap, s);
     i = 0;
-    while (s)
-    {
-        
-        if (*s == '%')
-        {
-            i++;
-            if (*s == c)
-                ft_putchar_ap(va_arg(s, ap));
-            else if (*s == s)
-                ft_putstr_ap(s, ap);
-            else if (*s == x || *s == X || *s == p)
-                ft_hexa(s, ap);
-            else if (*s == d || *s == i)
-                ft_putnubr_ap(s, ap);
-            else if (*s == u)
-                ft_putunsigned(s);
-        }
-        // il manque d, i, u
-        ft_putstr_ap(s);
-        s++;
-    }
+    if (*s == '%')
+{
+    s++; // Passer au caractère suivant après '%'
+    if (*s == 'c')
+        ft_putchar_ap(va_arg(ap, int));
+    else if (*s == 's')
+        ft_putstr_ap(va_arg(ap, char *));
+    else if (*s == 'x' || *s == 'X' || *s == 'p')
+        ft_hexa(va_arg(ap, unsigned long), *s);
+    else if (*s == 'd' || *s == 'i')
+        ft_putnbr_ap(va_arg(ap, int), 10); // '10' pour base décimale
+    else if (*s == 'u')
+        ft_putunsigned(va_arg(ap, unsigned long));
+}
     va_end(ap, s);
-    
-    // si le flag est c, utiliser ft_putchar
-    //si le flag est s, putstring
-    //si le flag est p --> custom fonction pour hexadecimal
-    //si le flag est d, putnbr
-    //si i le flag est i, putnbr
-    //si le le flag est u, faire iun putnbr juste pour positifs
-    // pour x et X custom fonction hexadecimale
-    //%% --> ft_putchar %
-    
 }
