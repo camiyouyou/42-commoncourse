7b2fe4865d882e856e8eee518e3833e409acb07d
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-07-16 19:24:09 +0200

    Update graphics_management.c

--- so_long/graphics_management.c
+++ so_long/graphics_management.c
@@ -80,14 +80,58 @@ int	exit_game(t_game *game)
 	exit(1);
 }
 
+void	info_map(char **map, t_game *game)
+{
+	int	y;
+	int	x;
+
+	game->nb_action = 0;
+	game->c_p = 0;
+	game->c_tot = 0;
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == 'C')
+				game->c_tot = game->c_tot + 1;
+			if (map[y][x] == 'P')
+			{
+				game->ply_x = x;
+				game->ply_y = y;
+			}
+			x++;
+		}
+		y++;
+	}
+	game->len_y = y;
+	game->len_x = x;
+}
+
 void	show_map(char **map)
 {
-	t_game *game;
+	t_game game;
 	int	width;
 	int	length;
 
-	texture_init(game);
-	info_map(map, game);
-	map_to_struct(map, game, 0, 0);
+	texture_init(&game);
+	info_map(map, &game);
+	map_to_struct(map, &game, 0, 0);
 	game.mlx = mlx_init();
+	game.wdw = mlx_new_window(game.mlx, game.len_x * 100, game len_y * 100, "so_long");
+	game.img_0 = mlx_xpm_file_to_image(game.mlx, game.path_0, &img_w, &img_h);
+	check_img(game.img_0);
+	game.img_1 = mlx_xpm_file_to_image(game.mlx, game.path_1, &img_w, &img_h);
+	check_img(game.img_1);
+	game.img_c = mlx_xpm_file_to_image(game.mlx, game.path_c, &img_w, &img_h);
+	check_img(game.img_c);
+	game.img_e = mlx_xpm_file_to_image(game.mlx, game.path_e, &img_w, &img_h);
+	check_img(game.img_e);
+	game.img_p = mlx_xpm_file_to_image(game.mlx, game.path_p, &img_w, &img_h);
+	check_img(game.img_p);
+	put_texture(&game, map);
+	mlx_key_hook(game.wdw, mng_input, &game);
+	mlx_hook(game.wdw, 17, 0, exit_game, &game);
+	mlx_loop(game.mlx);
 }
