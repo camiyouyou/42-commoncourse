f4b7948cac036f8789b0020f080bf6d8153cd0e3
camiyouyou <152023725+camiyouyou@users.noreply.github.com>
2024-07-23 14:37:00 +0200

    Update so_long.h

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -45,4 +45,27 @@ typedef struct game
 char	*get_next_line(int fd);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char *s1, char *s2);
+int	check_components(char **map, int i, int j, t_long *check);
+int	check_duplicate(int i);
+void	texture_init(t_game *game);
+void	check_img(void *img);
+void	map_to_struct(char **map, t_game *map_struct, int x, int y);
+int	mng_input(int key, t_game *game);
+int	exit_game(t_game *game);
+void	info_map(char **map, t_game *game);
+void	show_map(char **map);
+int	check_rectangle(char **map, int i, int j, int x);
+int	get_cols(char **map);
+int	check_walls(char **map, int i, int j);
+int	check_struct(char **map, int i, int j);
+int	check_exit(char **map, t_long *game, int x, int y);
+int	check_extension(char *name, int i);
+void	check_pos_e(char **map, t_long *game);
+void	check_pos_p(char **map, t_long *game);
+int	check_map_main(char **map, t_long *check);
+void	free_map(char **map);
+void	put_texture(t_game *game, char **map);
+char	**save_map(char **map, int fd);
+int	map_mng(int fd);
+
 
